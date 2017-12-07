f7fb4f7010f30f7373f333e5d1d725241177d334
Henrique Goetz <Henrique Goetz@DESKTOP-K2N43T8>
2017-12-07 13:17:06 -0200

    Corrigindo

--- src/Jogador.java
+++ src/Jogador.java
@@ -18,16 +18,16 @@ import javax.swing.JLabel;
  * @author Júlia
  */
 public class Jogador {
+
     private int vida;
     private int pontos;
-    private ImageIcon imagem; 
+    private ImageIcon imagem;
     private int altura;
     private int largura;
     private int posicaoHorizontal;
     private int posicaoVertical;
     private JLabel label;
 
-    
     public Jogador(JLabel label) throws IOException {
         BufferedImage imagem = ImageIO.read(new File("imagens/Sapo1.png"));
         this.setPosicao(300, 0);
@@ -35,17 +35,17 @@ public class Jogador {
         this.setLabel(label);
         this.setImagem(imagem.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
     }
-    
+
     public void setTamanho(int largura, int altura) {
         this.largura = largura;
         this.altura = altura;
     }
-    
+
     public void setPosicao(int x, int y) {
         this.posicaoHorizontal = x;
         this.posicaoVertical = y;
     }
-    
+
     public int getAltura() {
         return altura;
     }
@@ -66,55 +66,49 @@ public class Jogador {
         this.label = label;
     }
 
-   // public void setImagem(BufferedImage imagem) {
+    // public void setImagem(BufferedImage imagem) {
     public void setImagem(Image imagem) {
         ImageIcon imageIcon = new ImageIcon(imagem);
         this.imagem = imageIcon;
         label.setIcon(this.imagem);
     }
-    
-    public void moveEsquerda(){
-<<<<<<< HEAD
-        if(this.posicaoHorizontal != 0){
-=======
->>>>>>> 6e2fd4bab19ccac30d497d34d03d51bd7af2b8c5
-        this.posicaoHorizontal -= 20;
-        atualizaPosicao();
+
+    public void moveEsquerda() {
+
+        if (this.posicaoHorizontal != 0) {
+            this.posicaoHorizontal -= 20;
+            atualizaPosicao();
         }
     }
-    
-    public void moveDireita(){
-<<<<<<< HEAD
-        if(this.posicaoHorizontal!=580){
-=======
->>>>>>> 6e2fd4bab19ccac30d497d34d03d51bd7af2b8c5
-        this.posicaoHorizontal += 20;
-        atualizaPosicao();
+
+    public void moveDireita() {
+
+        if (this.posicaoHorizontal != 580) {
+            this.posicaoHorizontal += 20;
+            atualizaPosicao();
         }
     }
-    
-    public void moveCima(){
-<<<<<<< HEAD
-        if(this.posicaoVertical!=5){
-=======
->>>>>>> 6e2fd4bab19ccac30d497d34d03d51bd7af2b8c5
-        this.posicaoVertical++;
-        atualizaPosicao();
+
+    public void moveCima() {
+
+        if (this.posicaoVertical != 5) {
+            this.posicaoVertical++;
+            atualizaPosicao();
         }
     }
-    
-    public void moveBaixo(){
-<<<<<<< HEAD
-        if(this.posicaoVertical != 0){
+
+    public void moveBaixo() {
+
+        if (this.posicaoVertical != 0) {
             this.posicaoVertical--;
             atualizaPosicao();
         }
-=======
+
         this.posicaoVertical--;
         atualizaPosicao();
->>>>>>> 6e2fd4bab19ccac30d497d34d03d51bd7af2b8c5
     }
-    private void resetVida(){
+
+    private void resetVida() {
         this.setVida(3);
     }
 
@@ -133,22 +127,22 @@ public class Jogador {
     public void setPontos(int pontos) {
         this.pontos = pontos;
     }
-    
-    private void reduzVida(){
-        this.setVida(this.getVida()-1);
+
+    private void reduzVida() {
+        this.setVida(this.getVida() - 1);
     }
-    
-    private int somaPontos(int pontos){
+
+    private int somaPontos(int pontos) {
         System.out.println("soma pontos");
         return 0;
     }
-    
-    private boolean vivo(){
+
+    private boolean vivo() {
         System.out.println("retorna booleano");
         return true;
-    }  
-    
-    public void atualizaPosicao(){
-        label.setBounds(getPosicaoHorizontal(), 400-getPosicaoVertical()*75, getAltura(), getLargura());
+    }
+
+    public void atualizaPosicao() {
+        label.setBounds(getPosicaoHorizontal(), 400 - getPosicaoVertical() * 75, getAltura(), getLargura());
     }
 }
