bc4fb071ede9d5cc6a55b2d41ed5f7afc9e7d24d
mabergmann <32148891+mabergmann@users.noreply.github.com>
2017-12-08 00:01:33 -0200

    Sprites do jogador

--- src/Jogador.java
+++ src/Jogador.java
@@ -3,9 +3,13 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import static java.lang.Thread.sleep;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -28,13 +32,34 @@ public class Jogador {
     private int posicaoVertical;
     private JLabel label;
     private boolean vivo;
+    private boolean movendo = false;
+    private final Image frente1;
+    private final Image frente2;
+    private final Image baixo1;
+    private final Image baixo2;
+    private final Image esquerda1;
+    private final Image esquerda2;
+    private final Image esquerda3;
+    private final Image direita1;
+    private final Image direita2;
+    private final Image direita3;
 
     public Jogador(JLabel label) throws IOException {
-        BufferedImage imagem = ImageIO.read(new File("imagens/Sapo1.png"));
-        this.setPosicao(300, 0);
+        this.frente1 = ImageIO.read(new File("imagens/sapoFrente1.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        this.frente2 = ImageIO.read(new File("imagens/sapoFrente2.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        this.baixo1 = ImageIO.read(new File("imagens/sapoTras1.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        this.baixo2 = ImageIO.read(new File("imagens/sapoTras2.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        this.esquerda1 = ImageIO.read(new File("imagens/sapoEsquerda1.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        this.esquerda2 = ImageIO.read(new File("imagens/sapoEsquerda2.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        this.esquerda3 = ImageIO.read(new File("imagens/sapoEsquerda3.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        this.direita1 = ImageIO.read(new File("imagens/sapoDireita1.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        this.direita2 = ImageIO.read(new File("imagens/sapoDireita2.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        this.direita3 = ImageIO.read(new File("imagens/sapoDireita3.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+        
+        this.setPosicao(300, 400);
         this.setTamanho(50, 50);
         this.setLabel(label);
-        this.setImagem(imagem.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+        this.setImagem(frente1.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
         this.vivo = true;
     }
 
@@ -75,35 +100,118 @@ public class Jogador {
         label.setIcon(this.imagem);
     }
 
-    public void moveEsquerda() {
+    public void moveEsquerda() throws InterruptedException {
+        if (this.posicaoHorizontal > 40 && !this.movendo) {
+            this.movendo = true;
 
-        if (this.posicaoHorizontal != 0) {
-            this.posicaoHorizontal -= 20;
-            atualizaPosicao();
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    setImagem(esquerda2);
+                    for (int i = 0; i < 40; i++) {
+                        if (i == 20) {
+                            setImagem(esquerda3);
+                        }
+
+                        posicaoHorizontal -= 1;
+                        atualizaPosicao();
+                        try {
+                            Thread.sleep(5);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Jogador.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    setImagem(esquerda1);
+                }
+            });
+            thread.start();
+
+            this.movendo = false;
         }
+
     }
 
     public void moveDireita() {
 
-        if (this.posicaoHorizontal != 580) {
-            this.posicaoHorizontal += 20;
-            atualizaPosicao();
+        if (this.posicaoHorizontal < 560 && !this.movendo) {
+            this.movendo = true;
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    setImagem(direita2);
+                    for (int i = 0; i < 40; i++) {
+                        if (i == 20) {
+                            setImagem(direita3);
+                        }
+                        posicaoHorizontal += 1;
+                        atualizaPosicao();
+                        try {
+                            Thread.sleep(5);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Jogador.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    setImagem(direita1);
+                }
+            });
+            thread.start();
+
+            this.movendo = false;
         }
     }
 
     public void moveCima() {
 
-        if (this.posicaoVertical != 5) {
-            this.posicaoVertical++;
-            atualizaPosicao();
+        if (this.posicaoVertical > 40 && !this.movendo) {
+            this.movendo = true;
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    setImagem(frente2);
+                    for (int i = 0; i < 75; i++) {
+                        posicaoVertical -= 1;
+                        atualizaPosicao();
+                        try {
+                            Thread.sleep(5);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Jogador.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    setImagem(frente1);
+                }
+            });
+            thread.start();
+
+            this.movendo = false;
         }
     }
 
     public void moveBaixo() {
 
-        if (this.posicaoVertical != 0) {
-            this.posicaoVertical--;
-            atualizaPosicao();
+        if (this.posicaoVertical < 360 && !this.movendo) {
+            this.movendo = true;
+
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    setImagem(baixo2);
+                    for (int i = 0; i < 75; i++) {
+                        posicaoVertical++;
+                        atualizaPosicao();
+                        try {
+                            Thread.sleep(5);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Jogador.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                    setImagem(baixo1);
+                }
+            });
+            thread.start();
+
+            this.movendo = false;
         }
     }
 
@@ -141,6 +249,7 @@ public class Jogador {
     }
 
     public void atualizaPosicao() {
-        label.setBounds(getPosicaoHorizontal(), 400 - getPosicaoVertical() * 75, getAltura(), getLargura());
+        label.setBounds(getPosicaoHorizontal(), getPosicaoVertical(), getAltura(), getLargura());
+        label.setVisible(true);
     }
 }
